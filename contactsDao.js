6990501ac0dab2a0fe976c86f9d63264963fe92b
Phillip LeMaster <philalem21@gmail.com>
2020-10-14 22:32:49 -0400

    fixed the exports

--- contactsDao.js
+++ contactsDao.js
@@ -1,8 +1,8 @@
-var Datastore = require("nedb"),
-  db = new Datastore({ filename: "database", autoload: true });
+var Datastore = require("nedb");
+exports.db = new Datastore({ filename: "database", autoload: true });
 
 exports.getAllContacts = async (callback) => {
-  db.find({}, (err, docs) => {
+  exports.db.find({}, (err, docs) => {
     if (err) {
       console.log("error:" + err);
       return callback(err);
@@ -12,15 +12,15 @@ exports.getAllContacts = async (callback) => {
 };
 
 exports.addContact = async (contact) => {
-  db.update(contact, { $set: contact }, { upsert: true });
+  exports.db.insert(contact);
 };
 
 exports.updateContact = async (id, newContact) => {
-  db.update({ _id: id }, newContact);
+  exports.db.update({ _id: id }, newContact);
 };
 
 exports.getContact = async (id, callback) => {
-  return db.findOne({ _id: id }, (err, docs) => {
+  return exports.db.findOne({ _id: id }, (err, docs) => {
     if (err) {
       console.log("error:" + err);
       return callback(err);
@@ -30,7 +30,5 @@ exports.getContact = async (id, callback) => {
 };
 
 exports.deleteContact = async (id) => {
-  return db.remove({ _id: id });
+  return exports.db.remove({ _id: id });
 };
-
-module.exports = { db };
